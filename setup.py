592386c7144331e3fef66647c718597c6c08d521
konflic <konflic@mail.ru>
2025-03-30 12:17:11 +0300

    Codestyle for setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,19 @@ setup(
     version="1.0-4",
     packages=find_packages(),
     install_requires=[
-        'PyGObject',
+        "PyGObject",
     ],
     data_files=[
-        ('share/applications', ['data/python-runner.desktop']),
-        ('share/icons/hicolor/scalable/apps', ['data/icons/python-runner.svg']),
-        ('share/glib-2.0/schemas', ['data/glib-2.0/schemas/com.example.python-runner.gschema.xml']),
+        ("share/applications", ["data/python-runner.desktop"]),
+        ("share/icons/hicolor/scalable/apps", ["data/icons/python-runner.svg"]),
+        (
+            "share/glib-2.0/schemas",
+            ["data/glib-2.0/schemas/com.example.python-runner.gschema.xml"],
+        ),
     ],
     entry_points={
-        'console_scripts': [
-            'python_runner=python_runner.main:main',
+        "console_scripts": [
+            "python_runner=python_runner.main:main",
         ],
     },
 )
